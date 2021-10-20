53630c04bd3ab2a1c44000002912a8dc980e04b9
Mike Hotchkiss <michael.hotchkiss@protonmail.com>
2021-10-20 08:57:49 -0400

    removed unnecessary files and starter content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>This page is currently under construction</h1>
       </header>
     </div>
   );
